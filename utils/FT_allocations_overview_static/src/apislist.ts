aaeb03499831a663d2c2c483eb81ae91f78ed4a9
Gerardo Furtado <gerardofurtado@gmail.com>
2026-03-16 18:12:21 +1000

    New links

--- utils/FT_allocations_overview_static/src/apislist.ts
+++ utils/FT_allocations_overview_static/src/apislist.ts
@@ -6,7 +6,7 @@ type ApiFile = {
 
 export const apiFiles: ApiFile[] = [
 	{
-		url: "https://cbpfapi.unocha.org/vo2/odata/ProjectSummaryV2?",
+		url: "https://cbpfapib.unocha.org/vo2/odata/ProjectSummaryV2?",
 		name: "ProjectSummaryV2",
 		type: "yearly",
 	},
@@ -16,27 +16,27 @@ export const apiFiles: ApiFile[] = [
 		type: "yearly",
 	},
 	{
-		url: "https://cbpfapi.unocha.org/vo2/odata/MstPooledFund?$format=csv",
+		url: "https://cbpfapib.unocha.org/vo2/odata/MstPooledFund?$format=csv",
 		name: "MstPooledFund",
 		type: "complete",
 	},
 	{
-		url: "https://cbpfapi.unocha.org/vo2/odata/MstClusters?$format=csv",
+		url: "https://cbpfapib.unocha.org/vo2/odata/MstClusters?$format=csv",
 		name: "MstClusters",
 		type: "complete",
 	},
 	{
-		url: "https://cbpfapi.unocha.org/vo2/odata/MstOrgType?$format=csv",
+		url: "https://cbpfapib.unocha.org/vo2/odata/MstOrgType?$format=csv",
 		name: "MstOrgType",
 		type: "complete",
 	},
 	{
-		url: "https://cbpfapi.unocha.org/vo2/odata/MstAllocationSource?$format=csv",
+		url: "https://cbpfapib.unocha.org/vo2/odata/MstAllocationSource?$format=csv",
 		name: "MstAllocationSource",
 		type: "complete",
 	},
 	{
-		url: "https://cbpfapi.unocha.org/vo2/odata/AllocationTypes?PoolfundCodeAbbrv=&$format=csv",
+		url: "https://cbpfapib.unocha.org/vo2/odata/AllocationTypes?PoolfundCodeAbbrv=&$format=csv",
 		name: "AllocationTypes",
 		type: "complete",
 	},
